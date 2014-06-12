33913b94eb6b3cd374c34add4f1193c52cc3f434
eichmann <david-eichmann@uiowa.edu>
2014-06-12 20:04:56 +0000

    Added custom person iterators for the various roles to support filtering by year.

---
+++ src/main/java/ESTCTagLib/author/AuthorByYearIterator.java
@@ -0,0 +1,362 @@
+package ESTCTagLib.author;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.tagext.Tag;
+
+import ESTCTagLib.ESTCTagLibTagSupport;
+import ESTCTagLib.ESTCTagLibBodyTagSupport;
+import ESTCTagLib.record.Record;
+import ESTCTagLib.person.Person;
+
+@SuppressWarnings("serial")
+public class AuthorByYearIterator extends ESTCTagLibBodyTagSupport {
+    int ID = 0;
+    int pid = 0;
+    String year = null;
+    
+	Vector<ESTCTagLibTagSupport> parentEntities = new Vector<ESTCTagLibTagSupport>();
+
+	private static final Log log = LogFactory.getLog(AuthorIterator.class);
+
+
+    PreparedStatement stat = null;
+    ResultSet rs = null;
+    String sortCriteria = null;
+    int limitCriteria = 0;
+    String var = null;
+    int rsCount = 0;
+
+   boolean useRecord = false;
+   boolean usePerson = false;
+
+	public static String authorCountByPersonByYear(String pid, String yearString) throws JspTagException {
+		int count = 0;
+		int year = 0;
+		
+		if (yearString != null && !"".equals(yearString))
+			year = Integer.parseInt(yearString);
+		
+		AuthorIterator theIterator = new AuthorIterator();
+		try {
+			PreparedStatement stat = theIterator.getConnection().prepareStatement("SELECT count(*) from navigation.author natural join estc.pub_year where 1=1"
+						+ " and pid = ?"
+						+ (year == 0 ? "" : " and pubdate = ?")
+						);
+
+			stat.setInt(1,Integer.parseInt(pid));
+			if (year > 0)
+				stat.setInt(2, year);
+			ResultSet crs = stat.executeQuery();
+
+			if (crs.next()) {
+				count = crs.getInt(1);
+			}
+			stat.close();
+		} catch (SQLException e) {
+			log.error("JDBC error generating Author iterator", e);
+			throw new JspTagException("Error: JDBC error generating Author iterator");
+		} finally {
+			theIterator.freeConnection();
+		}
+		return "" + count;
+	}
+
+	public static Boolean personHasAuthorByYear(String pid, String year) throws JspTagException {
+		return ! authorCountByPersonByYear(pid, year).equals("0");
+	}
+
+    public int doStartTag() throws JspException {
+		Record theRecord = (Record)findAncestorWithClass(this, Record.class);
+		if (theRecord!= null)
+			parentEntities.addElement(theRecord);
+		Person thePerson = (Person)findAncestorWithClass(this, Person.class);
+		if (thePerson!= null)
+			parentEntities.addElement(thePerson);
+
+		if (theRecord == null) {
+		} else {
+			ID = theRecord.getID();
+		}
+		if (thePerson == null) {
+		} else {
+			pid = thePerson.getPid();
+		}
+
+
+      try {
+            //run count query  
+            int webapp_keySeq = 1;
+            stat = getConnection().prepareStatement("SELECT count(*) from " + generateFromClause() + " where 1=1"
+                                                        + generateJoinCriteria()
+                                                        + (ID == 0 ? "" : " and id = ?")
+                                                        + (pid == 0 ? "" : " and pid = ?")
+                                                        + (year == null ? "" : " and pubdate = ?")
+                                                        +  generateLimitCriteria());
+            if (ID != 0) stat.setInt(webapp_keySeq++, ID);
+            if (pid != 0) stat.setInt(webapp_keySeq++, pid);
+            if (year != null) stat.setInt(webapp_keySeq++, Integer.parseInt(year));
+            rs = stat.executeQuery();
+
+            if (rs.next()) {
+                pageContext.setAttribute(var+"Total", rs.getInt(1));
+            }
+
+
+            //run select id query  
+            webapp_keySeq = 1;
+            stat = getConnection().prepareStatement("SELECT navigation.author.id, navigation.author.pid from " + generateFromClause() + " where 1=1"
+                                                        + generateJoinCriteria()
+                                                        + (ID == 0 ? "" : " and id = ?")
+                                                        + (pid == 0 ? "" : " and pid = ?")
+                                                        + (year == null ? "" : " and pubdate = ?")
+                                                        + " order by " + generateSortCriteria() + generateLimitCriteria());
+            if (ID != 0) stat.setInt(webapp_keySeq++, ID);
+            if (pid != 0) stat.setInt(webapp_keySeq++, pid);
+            if (year != null) stat.setInt(webapp_keySeq++, Integer.parseInt(year));
+            rs = stat.executeQuery();
+            log.debug("AuthorByYear query: " + stat.toString());
+
+            if (rs.next()) {
+                ID = rs.getInt(1);
+                pid = rs.getInt(2);
+                pageContext.setAttribute(var, ID);
+                return EVAL_BODY_INCLUDE;
+            }
+        } catch (SQLException e) {
+            log.error("JDBC error generating Author iterator: " + stat.toString(), e);
+
+			freeConnection();
+			clearServiceState();
+
+			Tag parent = getParent();
+			if(parent != null){
+				pageContext.setAttribute("tagError", true);
+				pageContext.setAttribute("tagErrorException", e);
+				pageContext.setAttribute("tagErrorMessage", "Error: JDBC error generating Author iterator: " + stat.toString());
+				return parent.doEndTag();
+			}else{
+				throw new JspException("Error: JDBC error generating Author iterator: " + stat.toString(),e);
+			}
+
+        }
+
+        return SKIP_BODY;
+    }
+
+    private String generateFromClause() {
+       StringBuffer theBuffer = new StringBuffer("navigation.author");
+       theBuffer.append(", estc.pub_year");
+       if (useRecord)
+          theBuffer.append(", navigation.record");
+       if (usePerson)
+          theBuffer.append(", navigation.person");
+
+      return theBuffer.toString();
+    }
+
+    private String generateJoinCriteria() {
+       StringBuffer theBuffer = new StringBuffer();
+       theBuffer.append(" and pub_year.id = author.id");
+       if (useRecord)
+          theBuffer.append(" and record.ID = author.null");
+       if (usePerson)
+          theBuffer.append(" and person.pid = author.null");
+
+      return theBuffer.toString();
+    }
+
+    private String generateSortCriteria() {
+        if (sortCriteria != null) {
+            return sortCriteria;
+        } else {
+            return "pubdate";
+        }
+    }
+
+    private String generateLimitCriteria() {
+        if (limitCriteria > 0) {
+            return " limit " + limitCriteria;
+        } else {
+            return "";
+        }
+    }
+
+    public int doAfterBody() throws JspException {
+        try {
+            if (rs.next()) {
+                ID = rs.getInt(1);
+                pid = rs.getInt(2);
+                pageContext.setAttribute(var, ID);
+                return EVAL_BODY_AGAIN;
+            }
+        } catch (SQLException e) {
+            log.error("JDBC error iterating across Author", e);
+
+			freeConnection();
+			clearServiceState();
+
+			Tag parent = getParent();
+			if(parent != null){
+				pageContext.setAttribute("tagError", true);
+				pageContext.setAttribute("tagErrorException", e);
+				pageContext.setAttribute("tagErrorMessage", "JDBC error iterating across Author" + stat.toString());
+				return parent.doEndTag();
+			}else{
+				throw new JspException("JDBC error iterating across Author",e);
+			}
+
+        }
+        return SKIP_BODY;
+    }
+
+    public int doEndTag() throws JspTagException, JspException {
+        try {
+			if(pageContext != null){
+				Boolean error = (Boolean) pageContext.getAttribute("tagError");
+				if(error != null && error){
+
+					freeConnection();
+					clearServiceState();
+
+				Exception e = null; // (Exception) pageContext.getAttribute("tagErrorException");
+				String message = null; // (String) pageContext.getAttribute("tagErrorMessage");
+
+				if(pageContext != null){
+					e = (Exception) pageContext.getAttribute("tagErrorException");
+					message = (String) pageContext.getAttribute("tagErrorMessage");
+
+				}
+					Tag parent = getParent();
+					if(parent != null){
+						return parent.doEndTag();
+					}else if(e != null && message != null){
+						throw new JspException(message,e);
+					}else if(parent == null && pageContext != null){
+						pageContext.removeAttribute("tagError");
+						pageContext.removeAttribute("tagErrorException");
+						pageContext.removeAttribute("tagErrorMessage");
+					}
+				}
+			}
+            rs.close();
+            stat.close();
+        } catch (SQLException e) {
+            log.error("JDBC error ending Author iterator",e);
+			freeConnection();
+
+			Tag parent = getParent();
+			if(parent != null){
+				pageContext.setAttribute("tagError", true);
+				pageContext.setAttribute("tagErrorException", e);
+				pageContext.setAttribute("tagErrorMessage", "JDBC error retrieving ID " + ID);
+				return parent.doEndTag();
+			}else{
+				throw new JspException("Error: JDBC error ending Author iterator",e);
+			}
+
+        } finally {
+            clearServiceState();
+            freeConnection();
+        }
+        return super.doEndTag();
+    }
+
+    private void clearServiceState() {
+        ID = 0;
+        pid = 0;
+        year = null;
+        parentEntities = new Vector<ESTCTagLibTagSupport>();
+
+        this.rs = null;
+        this.stat = null;
+        this.sortCriteria = null;
+        this.var = null;
+        this.rsCount = 0;
+    }
+
+    public String getSortCriteria() {
+        return sortCriteria;
+    }
+
+    public void setSortCriteria(String sortCriteria) {
+        this.sortCriteria = sortCriteria;
+    }
+
+    public int getLimitCriteria() {
+        return limitCriteria;
+    }
+
+    public void setLimitCriteria(int limitCriteria) {
+        this.limitCriteria = limitCriteria;
+    }
+
+    public String getVar() {
+        return var;
+    }
+
+    public void setVar(String var) {
+        this.var = var;
+    }
+
+
+   public boolean getUseRecord() {
+        return useRecord;
+    }
+
+    public void setUseRecord(boolean useRecord) {
+        this.useRecord = useRecord;
+    }
+
+   public boolean getUsePerson() {
+        return usePerson;
+    }
+
+    public void setUsePerson(boolean usePerson) {
+        this.usePerson = usePerson;
+    }
+
+
+
+	public int getID () {
+		return ID;
+	}
+
+	public void setID (int ID) {
+		this.ID = ID;
+	}
+
+	public int getActualID () {
+		return ID;
+	}
+
+	public int getPid () {
+		return pid;
+	}
+
+	public void setPid (int pid) {
+		this.pid = pid;
+	}
+
+	public int getActualPid () {
+		return pid;
+	}
+
+	public String getYear () {
+		return year;
+	}
+
+	public void setYear (String year) {
+		if (!"".equals(year))
+			this.year = year;
+	}
+}
